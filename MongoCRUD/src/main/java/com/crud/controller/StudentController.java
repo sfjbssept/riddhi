cb8296028d0b6c6035b1e3c2d7fdf057cf7329b5
sfjbssept <sfjbssept@gmail.com>
2022-09-16 03:49:08 +0000

    update student on mongodb

--- MongoCRUD/src/main/java/com/crud/controller/StudentController.java
+++ MongoCRUD/src/main/java/com/crud/controller/StudentController.java
@@ -3,7 +3,9 @@ package com.crud.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +27,17 @@ public class StudentController {
 	@GetMapping("/getstudents")
 	public ResponseEntity<?> getStudents(){
 		return ResponseEntity.ok(this.studentRepository.findAll());
+	}
+	
+	@PutMapping("/updatestudent/{id}")
+	public ResponseEntity<Student> updateStudent(@PathVariable int id,@RequestBody Student student){
+		Student updatestudent = this.studentRepository.findById(id).get();
+		updatestudent.setCity(student.getCity());
+		updatestudent.setCollege(student.getCollege());
+		updatestudent.setId(student.getId());
+		updatestudent.setName(student.getName());
+		this.studentRepository.save(updatestudent);
+		return ResponseEntity.ok(updatestudent);
+		
 	}
 }
