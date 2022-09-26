33497b9870c4b55e307c9dd4162b6f323e900aa3
sfjbssept <sfjbssept@gmail.com>
2022-09-26 03:59:06 +0000

    testcases written

--- spring-security-rest/src/test/java/com/secure/controller/AppControllerTest.java
+++ spring-security-rest/src/test/java/com/secure/controller/AppControllerTest.java
@@ -1,12 +1,26 @@
 package com.secure.controller;
 
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.Base64Utils;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.secure.entity.Employee;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest({AppController.class})
@@ -40,7 +54,50 @@ public class AppControllerTest {
 	@Value("${user.user1.password}")
 	String admin_password;
 	
+	@Test
+	public void testEmployeeGet() throws Exception{
+		ResultActions responseEntity = processApiRequest(geturl,HttpMethod.GET,null,null,username1,password);
+		responseEntity.andExpect(status().isOk());
+		ObjectMapper mapper = new ObjectMapper();
+		String result = responseEntity.andReturn().getResponse().getContentAsString();
+		assertEquals("get employee", result);
+		
+	}
 	
+	@Test
+	public void testEmployeePost() throws Exception{
+		Employee employee = createEmployee("test","dev");
+		ResultActions responseEntity = processApiRequest(posturl,HttpMethod.POST,null,employee,admin_name1,admin_password);
+		responseEntity.andExpect(status().isOk());
+		ObjectMapper mapper = new ObjectMapper();
+		Employee result = mapper.readValue(responseEntity.andReturn().getResponse().getContentAsString(),new TypeReference<Employee>() {
+		});
+		assertEquals("test",result.getName());
+		assertEquals("dev",result.getRole());
+	}
+	
+	private Employee createEmployee(String string, String string2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private ResultActions processApiRequest(String api,HttpMethod methodtype,String name, Employee employee,
+			String username, String password) {
+		
+		ResultActions response = null;
+		String secret = "Basic" + Base64Utils.encodeToString((username+":"+password).getBytes());
+		try {
+			switch(methodtype) {
+			case GET:
+				response = mockMvc.perform(get(api).header(HttpHeaders.AUTHORIZATION,secret));
+				break;
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+		return response;
+	}
 	
 	
 	
